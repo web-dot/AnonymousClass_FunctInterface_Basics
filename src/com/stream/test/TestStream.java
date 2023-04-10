138236c8d882d99de3250aa431fe678a6e534428
sudipto <cg.sudipc@gmail.com>
2023-04-10 23:37:27 +0530

    stream basics

--- src/com/stream/test/TestStream.java
+++ src/com/stream/test/TestStream.java
@@ -1,14 +1,21 @@
 package com.stream.test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestStream {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		/**
+		 * source: https://www.bing.com/search?q=jav+stream+exercise&form=ANNTH1&refig=8f24045c35e744589bb8328c0767043e
+		 * */
 		
 		List<Product> productRepo = new ArrayList<>(Arrays.asList(
 				new Product(10, "book1", "Books", 30),
@@ -50,9 +57,39 @@ public class TestStream {
 				.collect(Collectors.toList());
 		System.out.println(toysPrices);
 		
+		/**
+		 * source: https://github.com/ltpitt/java-streams-exercises/blob/master/src/main/java/it/davidenastri/javastreamsexercises/StreamsExercise.java
+		 * */
 		
 		
+		Stream.of("Salvatore", "Piero", "Doneila", "Gaia", "Davide")
+		.sorted()
+		.findFirst()
+		.ifPresent(System.out::println);;
 		
+		
+		String[] names = {"Salvatore", "Piero", "Doneila", "Gaia", "Davide", "Daniel"};
+		Arrays.stream(names)
+		.filter(x -> x.startsWith("D"))
+		.sorted()
+		.forEach(System.out::println);;
+		
+		
+		Arrays.stream(new int[] {2,4,6,8,10})
+		.map(x -> x * x)
+		.average()
+		.ifPresent(System.out::println);
+		
+		List<String> people = Arrays.asList("Salvatore", "Piero", "Doneila", "Gaia", "Davide", "Daniel");
+		people.stream()
+		.map(String::toLowerCase)
+		.filter(x -> x.startsWith("d"))
+		.forEach(System.out::println);;
+		
+		Stream<String> bands = Files.lines(Paths.get("C:/Users/fedev/OneDrive/Documents/eclipse_workspaces/primary_workspace/StreamsApi/src/com/stream/bands.txt"));
+		bands.sorted()
+		.filter(x -> x.length() > 11)
+		.forEach(System.out::println);
 		
 	}
 }
